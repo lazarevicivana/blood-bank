27ab8107a9c3cd9d254224c5d6d37c8d332289df
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-10-21 13:48:19 +0200

    inital projects

---
+++ blood-bank-be/src/main/java/ftn/bloodbankbe/BloodBankBeApplication.java
@@ -0,0 +1,13 @@
+package ftn.bloodbankbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodBankBeApplication.class, args);
+    }
+
+}
---
+++ blood-bank-be/src/test/java/ftn/bloodbankbe/BloodBankBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.bloodbankbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
