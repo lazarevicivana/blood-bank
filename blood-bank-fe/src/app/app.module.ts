d86f3cf1d180f85dd9a4af9f678445e8f1b4865f
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2022-11-05 12:03:17 +0100

    initial navbar implementation

--- blood-bank-fe/src/app/app.module.ts
+++ blood-bank-fe/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { NavigationBarComponent } from './components/navigation-bar/navigation-b
     NavigationBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
