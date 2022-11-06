2725a750ad4a49ddb1ff04976bc31534a5c62e90
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2022-11-06 19:17:04 +0100

    initial implementation of account view and change

--- blood-bank-fe/src/app/app.module.ts
+++ blood-bank-fe/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
 import { IonicModule } from "@ionic/angular";
 import { AppRoutingModule } from "./app-routing.module";
-
+import {HttpClientModule,HttpHeaders} from '@angular/common/http'
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
@@ -12,6 +13,11 @@ import { BodyComponent } from './components/body/body.component';
 import { AllCentersComponent } from './view/all-centers/all-centers.component';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import { AccountComponent } from './view/account/account.component';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -29,7 +35,14 @@ import { AccountComponent } from './view/account/account.component';
     IonicModule.forRoot(),
     RouterOutlet,
     RouterLink,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blood-bank-fe/src/app/model/Customer.ts
@@ -0,0 +1,26 @@
+export interface Customer{
+  id: string,
+  username: string,
+  password: string,
+  name: string,
+  surname: string,
+  phone: string,
+  jmbg: string,
+  email: string,
+  userRole: string,
+  address: {
+    id: string,
+    city: string,
+    street: string,
+    country: string,
+    streetNumber: string
+  },
+  enabled: boolean,
+  deleted: boolean,
+  profession: {
+    id: string,
+    professionStatus: string,
+    professionDescription: string
+  },
+  "gender": string
+}
