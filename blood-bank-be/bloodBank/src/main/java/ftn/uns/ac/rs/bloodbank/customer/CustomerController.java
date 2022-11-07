6875c5b72ecf0641eba19fbe89ae87d55eca4a34
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2022-11-07 16:33:49 +0100

    cors policy resolve

--- blood-bank-be/bloodBank/src/main/java/ftn/uns/ac/rs/bloodbank/customer/CustomerController.java
+++ blood-bank-be/bloodBank/src/main/java/ftn/uns/ac/rs/bloodbank/customer/CustomerController.java
@@ -3,10 +3,7 @@ package ftn.uns.ac.rs.bloodbank.customer;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Console;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path="api/v1/customer")
 public class CustomerController {
     private final CustomerService customerService;
