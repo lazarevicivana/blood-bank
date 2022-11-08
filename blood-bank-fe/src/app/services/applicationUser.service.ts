ad04c9c29eb81bd300429226c0d70658392720f7
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2022-11-08 13:53:19 +0100

    addong put method to fron

---
+++ blood-bank-fe/src/app/services/applicationUser.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
+import {ApplicationUser} from "../model/ApplicationUser";
+
+const httpOptions ={
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationUserService {
+  private aplUrl = 'http://localhost:8080/api/v1/applicationUser'
+
+  constructor(private http:HttpClient) { }
+
+  getApplicationUserById(): Observable<ApplicationUser>{
+    const url = `${this.aplUrl}/34713840-ddf3-49b2-9cae-47334cb6b31b`
+
+    // @ts-ignore
+    return this.http.get(url)
+
+  }
+
+  updateApplicationUser(user: ApplicationUser):Observable<ApplicationUser>{
+
+    console.log(user);
+    // @ts-ignore
+    return this.http.put(this.aplUrl,user,httpOptions);
+  }
+}
