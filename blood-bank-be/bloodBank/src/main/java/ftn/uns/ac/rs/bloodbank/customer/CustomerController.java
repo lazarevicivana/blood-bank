69466a8fc0c03150717fe1dd3e1a8efb00842027
Mihaela Osmajić <elaosmajic@gmail.com>
2022-11-12 19:26:12 +0100

    add data and search donors BE

--- blood-bank-be/bloodBank/src/main/java/ftn/uns/ac/rs/bloodbank/customer/CustomerController.java
+++ blood-bank-be/bloodBank/src/main/java/ftn/uns/ac/rs/bloodbank/customer/CustomerController.java
@@ -1,12 +1,11 @@
 package ftn.uns.ac.rs.bloodbank.customer;
 
+import ftn.uns.ac.rs.bloodbank.customer.dto.CustomerSearchDto;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,5 +24,28 @@ public class CustomerController {
         return customerService.getById(id);
 //        return  customerService.getById(id);}
 
+    }
+    @GetMapping(value = "/getCenterDonors/{centerId}" )
+    public List<Customer> getCenterDonors(@PathVariable("centerId") UUID centerId)
+    {
+        return customerService.getCenterDonors(centerId);
+    }
+
+    @GetMapping(value = "/searchCenterDonors/{centerId}")
+    public List<Customer> searchCenterDonors(@PathVariable("centerId") UUID centerId, @RequestBody CustomerSearchDto dto)
+    {
+        return customerService.searchCenterDonors(centerId,dto);
+    }
+
+    @GetMapping(path = "/getDonors" )
+    public List<Customer> getDonors()
+    {
+        return customerService.getDonors();
+    }
+
+    @GetMapping(path = "/searchDonors")
+    public List<Customer> searchDonors(@RequestBody CustomerSearchDto dto)
+    {
+        return customerService.searchDonors(dto);
     }
 }
--- blood-bank-be/bloodBank/src/main/java/ftn/uns/ac/rs/bloodbank/customer/CustomerService.java
+++ blood-bank-be/bloodBank/src/main/java/ftn/uns/ac/rs/bloodbank/customer/CustomerService.java
@@ -1,16 +1,19 @@
 package ftn.uns.ac.rs.bloodbank.customer;
 
+import ftn.uns.ac.rs.bloodbank.appointment.model.ScheduleAppointment;
+import ftn.uns.ac.rs.bloodbank.appointment.repository.ScheduleAppointmentRepository;
+import ftn.uns.ac.rs.bloodbank.customer.dto.CustomerSearchDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final ScheduleAppointmentRepository scheduleAppointmentRepository;
 
     public List<Customer> getAllCustomers(){
         return customerRepository.findAll();
@@ -23,6 +26,25 @@ public class CustomerService {
                 .orElseThrow(() -> new RuntimeException("Customer with id:" + id + "does not exist"));
     }
 
+    public List<Customer> getCenterDonors(UUID centerID)
+    {
+        LocalDateTime now = LocalDateTime.now();
+        return scheduleAppointmentRepository.GetCenterDonors(centerID,now);
+    }
 
+    public List<Customer> searchCenterDonors(UUID centerId,CustomerSearchDto dto)
+    {
+        LocalDateTime now = LocalDateTime.now();
+        return scheduleAppointmentRepository.SearchCenterDonors(centerId, now, dto.getSearchName().toLowerCase(), dto.getSearchSurname().toLowerCase());
+    }
 
+    public List<Customer> getDonors() {
+        LocalDateTime now = LocalDateTime.now();
+        return scheduleAppointmentRepository.GetDonors(now);
+    }
+
+    public List<Customer> searchDonors(CustomerSearchDto dto) {
+        LocalDateTime now = LocalDateTime.now();
+        return scheduleAppointmentRepository.SearchDonors(now, dto.getSearchName().toLowerCase(), dto.getSearchSurname().toLowerCase());
+    }
 }
