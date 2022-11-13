60e5a7b083758c3233099acd11102cb0a521432d
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-11-13 22:51:47 +0100

    feature: nav-staff

--- blood-bank-fe/src/app/components/navigation-bar/nav-data/nav-data-stuff.ts
+++ blood-bank-fe/src/app/components/navigation-bar/nav-data/nav-data-stuff.ts
@@ -1,26 +1,20 @@
 export const stuffNavData=[
   {
-    routeLink: 'facilities',
-    icon: 'business-outline',
-    label: 'Facilities',
-    num:0
-  },
-  {
     routeLink: 'admin-center-profile',
     icon: 'home-outline',
     label: 'Center profile',
-    num:1
+    num:0
   },
   {
     routeLink: 'add-appointment',
     icon: 'add-circle-outline',
     label: 'Add appointment',
-    num:2
+    num:1
   },
   {
     routeLink: 'center-donors',
     icon: 'accessibility-outline',
     label: 'Center donors',
-    num:3
+    num:2
   }
 ];
