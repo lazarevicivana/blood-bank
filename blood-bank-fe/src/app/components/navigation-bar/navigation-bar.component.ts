a0d9d5c1b74fbeae1bfa7bb9d970e8ff24548750
Ivana Lazarevic <93544830+lazarevicivana@users.noreply.github.com>
2022-11-13 19:04:49 +0100

    unauthenticated users view

--- blood-bank-fe/src/app/components/navigation-bar/navigation-bar.component.ts
+++ blood-bank-fe/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -44,7 +44,6 @@ export class NavigationBarComponent implements OnInit {
     this.screenWidth = window.innerWidth;
     const user = this.tkStorage.getUser();
       this.userRole = user.role;
-      console.log(this.userRole);
   }
 
   clicked(num: number) {
